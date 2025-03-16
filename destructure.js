168d1ac8a01bbb93fb7b9518af3eefb2b2ec0db2
billybahirwe <billybillybahirwe@gmail.com>
2025-03-16 12:52:23 -0700

    Added validation.js to JAVASCRIPT-practice

---
+++ destructure.js
@@ -0,0 +1,15 @@
+let produce ={
+    name:"maize",
+    price:2000,
+    category:"grain"
+}
+
+const produceName = produce.name;
+const producePrice = produce.price;
+const produceCategory = produce.category;
+
+
+
+console.log(`${produceName}+ is +${produceCategory}`)
+// let {name,price,category} = produce
+// console.log(`${produceName}+ is +${produceCategory}`)
